b166cf4af1a2fee1ce7c7c223f83aa6380d1d89c
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-09-29 14:33:14 +0530

    update Home.jsx

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
-import HeroSection from './HeroSection'
-import CategoryCarousel from './CategoryCarousel'
-import LatestJobs from './LatestJobs'
-import Footer from './shared/Footer'
-import useGetAllJobs from '@/hooks/useGetAllJobs'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection";
+import CategoryCarousel from "./CategoryCarousel";
+import LatestJobs from "./LatestJobs";
+import Footer from "./shared/Footer";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  // fetch all jobs
   useGetAllJobs();
-  const { user } = useSelector(store => store.auth);
+
+  const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+
   useEffect(() => {
-    if (user?.role === 'recruiter') {
+    if (user?.role === "recruiter") {
       navigate("/admin/companies");
     }
-  }, []);
+  }, [user, navigate]); // ✅ added dependencies
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <HeroSection />
-      <CategoryCarousel />
-      <LatestJobs />
+      <main className="flex-grow">
+        <HeroSection />
+        <CategoryCarousel />
+        <LatestJobs />
+      </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
